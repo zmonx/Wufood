77c7f8c6ab976b58523d2bc74e88a0e6953785b5
dragon260961 <dragonsas1234@gmail.com>
2020-10-07 19:19:49 +0700

    addIcon

--- Screen/LoginScreen.js
+++ Screen/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   TextInput,
@@ -16,125 +16,120 @@ import {
 // import { Button } from 'react-native';
 import RegisterScreen from './RegisterScreen';
 
- export default class LoginScreen extends React.Component {
-    // Setting up Login Activity title.
-   
-  
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        UserEmail: "",
-        UserPassword: "",
-      };
-    }
-  
-    UserLoginFunction = () => {
-      const { UserEmail } = this.state;
-      const { UserPassword } = this.state;
-  
-      fetch("http://172.16.240.194/wufoodapi/login_api.php", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          user_email: UserEmail,
-  
-          password: UserPassword,
-        }),
-      })
-        .then((response) => response.json())
-        .then((responseJson) => {
-          // If server response message same as Data Matched
-          if (responseJson === "Data Matched") {
-            //Then open Profile activity and send user email to profile activity.
-            this.props.navigation.navigate("DrawerNavigationRoutes");
-            // props.navigation.navigate('DrawerNavigationRoutes');
-          } else {
-            Alert.alert(responseJson);
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+export default class LoginScreen extends React.Component {
+  // Setting up Login Activity title.
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      UserEmail: '',
+      UserPassword: '',
     };
-
-    
-    render() {
-      return (
-        <ImageBackground
-          source={require("../Image/backhome.png")}
-          style={{ flex: 1, resizeMode: "cover", justifyContent: "center" }}
-        >
-          <View style={styles.mainBody}>
-            {/* <Loader loading={loading} /> */}
-  
-            <ScrollView keyboardShouldPersistTaps="handled">
-              <View style={{ marginTop: 100 }}>
-                <KeyboardAvoidingView enabled>
-                  <View style={{ alignItems: "center" }}>
-                    <Image
-                      source={require("../Image/logo2.png")}
-                      style={{
-                        paddingTop: -30,
-                        //   width: '50%',
-                        //   height: '50%',
-                        //   // resizeMode: 'contain',
-                        //   // margin: 30,
-                      }}
-                    />
-                  </View>
-                  <View style={styles.SectionStyle}>
-                    <TextInput
-                      style={styles.inputStyle}
-                      onChangeText={(UserEmail) => this.setState({ UserEmail })}
-                      underlineColorAndroid="transparent"
-                      placeholder="Enter Email" //dummy@abc.com
-                      placeholderTextColor="#F6F6F7"
-                      autoCapitalize="none"
-                    />
-                  </View>
-  
-                  <View style={styles.SectionStyle}>
-                    <TextInput
-                      style={styles.inputStyle}
-                      onChangeText={(UserPassword) =>
-                        this.setState({ UserPassword })
-                      }
-                      underlineColorAndroid="transparent"
-                      placeholder="Enter Password" //12345
-                      placeholderTextColor="#F6F6F7"
-                      keyboardType="default"
-                      secureTextEntry={true}
-                    />
-                  </View>
-  
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    activeOpacity={0.5}
-                    onPress={this.UserLoginFunction}
-                  >
-                    <Text style={styles.buttonTextStyle}>LOGIN</Text>
-                  </TouchableOpacity>
-                  
-                   <Text
-              style={styles.registerTextStyle}
-              onPress={() => this.props.navigation.navigate('RegisterScreen')}>
-              New Here ? 
-              Register
-            </Text>
-                </KeyboardAvoidingView>
-              </View>
-            </ScrollView>
-          </View>
-        </ImageBackground>
-      );
-    }
   }
-  
 
+  UserLoginFunction = () => {
+    const {UserEmail} = this.state;
+    const {UserPassword} = this.state;
+
+    fetch('http://172.16.240.197/wufoodapi/login_api.php', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_email: UserEmail,
+
+        password: UserPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // If server response message same as Data Matched
+        if (responseJson === 'Data Matched') {
+          //Then open Profile activity and send user email to profile activity.
+          this.props.navigation.navigate('DrawerNavigationRoutes');
+          // props.navigation.navigate('DrawerNavigationRoutes');
+        } else {
+          Alert.alert(responseJson);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+      <ImageBackground
+        source={require('../Image/backhome.png')}
+        style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}>
+        <View style={styles.mainBody}>
+          {/* <Loader loading={loading} /> */}
+
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <View style={{marginTop: 100}}>
+              <KeyboardAvoidingView enabled>
+                <View style={{alignItems: 'center'}}>
+                  <Image
+                    source={require('../Image/logo2.png')}
+                    style={{
+                      paddingTop: -30,
+                      //   width: '50%',
+                      //   height: '50%',
+                      //   // resizeMode: 'contain',
+                      //   // margin: 30,
+                    }}
+                  />
+                </View>
+                <View style={styles.SectionStyle}>
+                  <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(UserEmail) => this.setState({UserEmail})}
+                    underlineColorAndroid="transparent"
+                    placeholder="Enter Email" //dummy@abc.com
+                    placeholderTextColor="#F6F6F7"
+                    autoCapitalize="none"
+                  />
+                </View>
+
+                <View style={styles.SectionStyle}>
+                  <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(UserPassword) =>
+                      this.setState({UserPassword})
+                    }
+                    underlineColorAndroid="transparent"
+                    placeholder="Enter Password" //12345
+                    placeholderTextColor="#F6F6F7"
+                    keyboardType="default"
+                    secureTextEntry={true}
+                  />
+                </View>
+
+                <TouchableOpacity
+                  style={styles.buttonStyle}
+                  activeOpacity={0.5}
+                  onPress={this.UserLoginFunction}>
+                  <Text style={styles.buttonTextStyle}>LOGIN</Text>
+                </TouchableOpacity>
+
+                <Text
+                  style={styles.registerTextStyle}
+                  onPress={() =>
+                    this.props.navigation.navigate('RegisterScreen')
+                  }>
+                  New Here ? Register
+                </Text>
+              </KeyboardAvoidingView>
+            </View>
+          </ScrollView>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   mainBody: {
--- Screen/drawerScreens/Cart.js
+++ Screen/drawerScreens/Cart.js
@@ -18,95 +18,132 @@ export default class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: '',
+      dataCart: [],
     };
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('cart').then((cart) => {
+      if (cart !== null) {
+        // We have data!!
+        const cartfood = JSON.parse(cart);
+        this.setState({dataCart: cartfood});
+      }
+    });
+    // .catch((err) => {
+    //   alert(err);
+    // });
   }
 
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <View style={{height: 20}} />
-        <Text style={{fontSize: 28, color: 'gray'}}>Cart food</Text>
+        <Text style={{fontSize: 32, fontWeight: 'bold', color: '#33c37d'}}>
+          Cart food
+        </Text>
         <View style={{height: 10}} />
 
-        <View style={{flex: 1}}>
-          <View
-            style={{
-              width: width - 20,
-              margin: 10,
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
-              borderBottomWidth: 2,
-              borderColor: '#cccccc',
-              paddingBottom: 10,
-            }}>
-            <Image
-              resizeMode={'contain'}
-              style={{width: width / 3, height: width / 3}}
-              source={{
-                uri: 'http://tutofox.com/foodapp/food/pizza/pizza-1.png',
-              }}
-            />
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                padding: 10,
-                justifyContent: 'space-between',
-              }}>
-              <View>
-                <Text style={{fontWeight: 'bold', fontSize: 20}}>
-                  Titulo de producto
-                </Text>
-                <Text>Descripcion de food</Text>
-              </View>
-              <View
-                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text
-                  style={{fontWeight: 'bold', color: '#9fd236', fontSize: 20}}>
-                  $565
-                </Text>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <TouchableOpacity>
-                    <Icon
-                      name="ios-remove-circle"
-                      size={30}
-                      color={'#9fd236'}
-                    />
-                  </TouchableOpacity>
-                  <Text style={{paddingHorizontal: 8, fontWeight: 'bold'}}>
-                    5
-                  </Text>
-                  <TouchableOpacity>
-                    <Icon name="ios-add-circle" size={30} color={'#9fd236'} />
-                  </TouchableOpacity>
+        <View style={{backgroundColor: 'transparent', flex: 1}}>
+          <ScrollView>
+            {this.state.dataCart.map((item) => {
+              return (
+                <View
+                  style={{
+                    width: width - 20,
+                    margin: 10,
+                    backgroundColor: 'transparent',
+                    flexDirection: 'row',
+                    borderBottomWidth: 2,
+                    borderColor: '#cccccc',
+                    paddingBottom: 10,
+                  }}>
+                  <Image
+                    resizeMode={'contain'}
+                    style={{width: width / 3, height: width / 3}}
+                    source={{uri: item.food.image}}
+                  />
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: 'trangraysparent',
+                      padding: 10,
+                      justifyContent: 'space-between',
+                    }}>
+                    <View>
+                      <Text style={{fontWeight: 'bold', fontSize: 20}}>
+                        {item.food.name}
+                      </Text>
+                      <Text>Lorem Ipsum de food</Text>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                      }}>
+                      <Text
+                        style={{
+                          fontWeight: 'bold',
+                          color: '#33c37d',
+                          fontSize: 20,
+                        }}>
+                        ${item.price * item.quantity}
+                      </Text>
+                      <View
+                        style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <TouchableOpacity>
+                          <Icon
+                            name="ios-remove-circle"
+                            size={35}
+                            color={'#33c37d'}
+                          />
+                        </TouchableOpacity>
+                        <Text
+                          style={{
+                            paddingHorizontal: 8,
+                            fontWeight: 'bold',
+                            fontSize: 18,
+                          }}>
+                          5
+                        </Text>
+                        <TouchableOpacity>
+                          <Icon
+                            name="ios-add-circle"
+                            size={35}
+                            color={'#33c37d'}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
                 </View>
-              </View>
-            </View>
-          </View>
+              );
+            })}
+
+            <View style={{height: 20}} />
+
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#33c37d',
+                width: width - 40,
+                alignItems: 'center',
+                padding: 10,
+                borderRadius: 5,
+                margin: 20,
+              }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: 'bold',
+                  color: 'white',
+                }}>
+                CHECKOUT
+              </Text>
+            </TouchableOpacity>
+
+            <View style={{height: 20}} />
+          </ScrollView>
         </View>
-
-        <View style={{height: 20}} />
-
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#9fd236',
-            width: width - 40,
-            alignItems: 'center',
-            padding: 10,
-            borderRadius: 5,
-          }}>
-          <Text
-            style={{
-              fontSize: 24,
-              fontWeight: 'bold',
-              color: 'white',
-            }}>
-            CHECKOUT
-          </Text>
-        </TouchableOpacity>
-
-        <View style={{height: 20}} />
       </View>
     );
   }
