aa460f94b45326cff942ac7cbd6077c7a789fe5d
wic0144 <arm_ruttanard@hotmail.com>
2021-03-22 14:31:24 +0700

    sum

--- Screen/Components/Modals.js
+++ Screen/Components/Modals.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import React, {Component} from 'react';
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
 
-class Modal extends Component {
+export default class Modals extends Component {
   state = {
-    modalVisible: false
+    modalVisible: false,
   };
 
   setModalVisible = (visible) => {
-    this.setState({ modalVisible: visible });
-  }
+    this.setState({modalVisible: visible});
+  };
 
   render() {
-    const { modalVisible } = this.state;
+    const {modalVisible} = this.state;
+    
     return (
       <View style={styles.centeredView}>
         <Modal
@@ -19,26 +20,30 @@ class Modal extends Component {
           transparent={true}
           visible={modalVisible}
           onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
+            Alert.alert('Modal has been closed.');
             this.setModalVisible(!modalVisible);
-          }}
-        >
+          }}>
+
+
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
               <Text style={styles.modalText}>Hello World!</Text>
               <Pressable
                 style={[styles.button, styles.buttonClose]}
-                onPress={() => this.setModalVisible(!modalVisible)}
-              >
+                onPress={() => this.setModalVisible(!modalVisible)}>
                 <Text style={styles.textStyle}>Hide Modal</Text>
               </Pressable>
             </View>
           </View>
+
+
+
         </Modal>
+
+
         <Pressable
           style={[styles.button, styles.buttonOpen]}
-          onPress={() => this.setModalVisible(true)}
-        >
+          onPress={() => this.setModalVisible(true)}>
           <Text style={styles.textStyle}>Show Modal</Text>
         </Pressable>
       </View>
@@ -49,45 +54,43 @@ class Modal extends Component {
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
   },
   modalView: {
     margin: 20,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
   button: {
     borderRadius: 20,
     padding: 10,
-    elevation: 2
+    elevation: 2,
   },
   buttonOpen: {
-    backgroundColor: "#F194FF",
+    backgroundColor: '#F194FF',
   },
   buttonClose: {
-    backgroundColor: "#2196F3",
+    backgroundColor: '#2196F3',
   },
   textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   modalText: {
     marginBottom: 15,
-    textAlign: "center"
-  }
+    textAlign: 'center',
+  },
 });
-
-export default Modal;
--- Screen/drawerScreens/Cart.js
+++ Screen/drawerScreens/Cart.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, Image, TouchableOpacity, ImageBackground, Dimensions,ScrollView } from 'react-native';
-var { width } = Dimensions.get("window")
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
+  ScrollView,
+  Pressable,
+  StyleSheet,
+  Modal,
+} from 'react-native';
+var {width} = Dimensions.get('window');
 // import icons
 import Icon from 'react-native-vector-icons/Ionicons';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -9,193 +21,255 @@ export default class Cart extends Component {
     super(props);
     this.state = {
       dataCart: [],
+      modalVisible: false,
+      total: 0,
     };
   }
 
-
-
-  componentDidMount()
-  {
-    AsyncStorage.getItem('cart').then((cart)=>{
-      if (cart !== null) {
-        // We have data!!
-        const cartfood = JSON.parse(cart)
-        this.setState({dataCart:cartfood})
-      }
-    })
-    .catch((err)=>{
-      alert(err)
-    })
+  setModalVisible = (visible) => {
+    this.setState({modalVisible: visible});
+  };
+  componentDidMount() {
+    AsyncStorage.getItem('cart')
+      .then((cart) => {
+        if (cart !== null) {
+          // We have data!!
+          const cartfood = JSON.parse(cart);
+          this.setState({dataCart: cartfood});
+          this.state.dataCart.map((item, i) => {
+            this.setState({
+              total: this.state.total + item.price * item.quantity,
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
 
-  onClickAddCart(data){
-
+  onClickAddCart(data) {
     const itemcart = {
       food: data,
-      quantity:  1,
-      price: data.price
-    }
- 
-    AsyncStorage.getItem('cart').then((datacart)=>{
+      quantity: 1,
+      price: data.price,
+    };
+
+    AsyncStorage.getItem('cart')
+      .then((datacart) => {
         if (datacart !== null) {
           // We have data!!
-          const cart = JSON.parse(datacart)
-          cart.push(itemcart)
-          AsyncStorage.setItem('cart',JSON.stringify(cart));
+          const cart = JSON.parse(datacart);
+          cart.push(itemcart);
+          AsyncStorage.setItem('cart', JSON.stringify(cart));
+        } else {
+          const cart = [];
+          cart.push(itemcart);
+          AsyncStorage.setItem('cart', JSON.stringify(cart));
         }
-        else{
-          const cart  = []
-          cart.push(itemcart)
-          AsyncStorage.setItem('cart',JSON.stringify(cart));
-        }
-        alert("Add Cart")
+        alert('Add Cart');
       })
-      .catch((err)=>{
-        alert(err)
-      })
+      .catch((err) => {
+        alert(err);
+      });
   }
-  onChangeQual(i,type)
-  {
-    const dataCar = this.state.dataCart
+  onChangeQual(i, type) {
+    const dataCar = this.state.dataCart;
     let cantd = dataCar[i].quantity;
 
     if (type) {
-     cantd = cantd + 1
-     dataCar[i].quantity = cantd
-     this.setState({dataCart:dataCar})
+      cantd = cantd + 1;
+      dataCar[i].quantity = cantd;
+      this.setState({dataCart: dataCar});
+    } else if (type == false && cantd >= 2) {
+      cantd = cantd - 1;
+      dataCar[i].quantity = cantd;
+      this.setState({dataCart: dataCar});
+    } else if (type == false && cantd == 1) {
+      dataCar.splice(i, 1);
+      this.setState({dataCart: dataCar});
     }
-    else if (type==false&&cantd>=2){
-     cantd = cantd - 1
-     dataCar[i].quantity = cantd
-     this.setState({dataCart:dataCar})
-    }
-    else if (type==false&&cantd==1){
-     dataCar.splice(i,1)
-     this.setState({dataCart:dataCar})
-    } 
   }
- 
+
   render() {
+    const {modalVisible, total} = this.state;
     return (
       <ImageBackground
-      source={require('./img/backhome2.png')}
-      style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <View style={{height: 20}} />
-        <Text style={{fontSize: 32, fontWeight: 'bold', color: '#000'}}>
-          Cart food
-        </Text>
-        <View style={{height: 10}} />
+        source={require('./img/backhome2.png')}
+        style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={{height: 20}} />
+          <Text style={{fontSize: 32, fontWeight: 'bold', color: '#000'}}>
+            Cart food
+          </Text>
+          <View style={{height: 10}} />
 
-        <View style={{flex: 1}}>
-          <ScrollView>
-            {this.state.dataCart.map((item, i) => {
-              return (
-                <View
-                  style={{
-                    width: width - 20,
-                    margin: 10,
-                    backgroundColor: 'transparent',
-                    flexDirection: 'row',
-                    borderBottomWidth: 2,
-                    borderColor: '#cccccc',
-                    paddingBottom: 10,
-                  }}>
-                  <Image
-                    resizeMode={'contain'}
-                    style={{width: width / 3, height: width / 3}}
-                    source={{uri: item.food.image}}
-                  />
+          <View style={{flex: 1}}>
+            <ScrollView>
+              {this.state.dataCart.map((item, i) => {
+                return (
                   <View
                     style={{
-                      flex: 1,
-                      backgroundColor: 'trangraysparent',
-                      padding: 10,
-                      justifyContent: 'space-between',
+                      width: width - 20,
+                      margin: 10,
+                      backgroundColor: 'transparent',
+                      flexDirection: 'row',
+                      borderBottomWidth: 2,
+                      borderColor: '#cccccc',
+                      paddingBottom: 10,
                     }}>
-                    <View>
-                      <Text style={{fontWeight: 'bold', fontSize: 20}}>
-                        {item.food.name}
-                      </Text>
-                      <Text>Lorem Ipsum de food</Text>
-                    </View>
+                    <Image
+                      resizeMode={'contain'}
+                      style={{width: width / 3, height: width / 3}}
+                      source={{uri: item.food.image}}
+                    />
                     <View
                       style={{
-                        flexDirection: 'row',
+                        flex: 1,
+                        backgroundColor: 'trangraysparent',
+                        padding: 10,
                         justifyContent: 'space-between',
                       }}>
-                      <Text
-                        style={{
-                          fontWeight: 'bold',
-                          color: '#000',
-                          fontSize: 20,
-                        }}>
-                        ${item.price * item.quantity}
-                      </Text>
+                      <View>
+                        <Text style={{fontWeight: 'bold', fontSize: 20}}>
+                          {item.food.name}
+                        </Text>
+                        <Text>Lorem Ipsum de food</Text>
+                      </View>
                       <View
-                        style={{flexDirection: 'row', alignItems: 'center'}}>
-                        <TouchableOpacity
-                          onPress={() => this.onChangeQual(i, false)}>
-                          <Icon
-                            name="ios-remove-circle"
-                            size={35}
-                            color={'#7f3166'}
-                          />
-                        </TouchableOpacity>
+                        style={{
+                          flexDirection: 'row',
+                          justifyContent: 'space-between',
+                        }}>
                         <Text
                           style={{
-                            paddingHorizontal: 8,
                             fontWeight: 'bold',
-                            fontSize: 18,
+                            color: '#000',
+                            fontSize: 20,
                           }}>
-                          {item.quantity}
+                          ${item.price * item.quantity}
                         </Text>
-                        <TouchableOpacity
-                          onPress={() => this.onChangeQual(i, true)}>
-                          <Icon
-                            name="ios-add-circle"
-                            size={35}
-                            color={'#7f3166'}
-                          />
-                        </TouchableOpacity>
+                        <View
+                          style={{flexDirection: 'row', alignItems: 'center'}}>
+                          <TouchableOpacity
+                            onPress={() => this.onChangeQual(i, false)}>
+                            <Icon
+                              name="ios-remove-circle"
+                              size={35}
+                              color={'#7f3166'}
+                            />
+                          </TouchableOpacity>
+                          <Text
+                            style={{
+                              paddingHorizontal: 8,
+                              fontWeight: 'bold',
+                              fontSize: 18,
+                            }}>
+                            {item.quantity}
+                          </Text>
+                          <TouchableOpacity
+                            onPress={() => this.onChangeQual(i, true)}>
+                            <Icon
+                              name="ios-add-circle"
+                              size={35}
+                              color={'#7f3166'}
+                            />
+                          </TouchableOpacity>
+                        </View>
                       </View>
                     </View>
                   </View>
-                </View>
-              );
-            })}
+                );
+              })}
 
-            <View style={{height: 20}} />
+              <View style={{height: 20}} />
 
-            <TouchableOpacity
-                // onPress={alert("CheckOut Done")}
-          
-              style={{
-                backgroundColor: '#7f3166',
-                width: width - 40,
-                alignItems: 'center',
-                padding: 10,
-                borderRadius: 5,
-                margin: 20,
-              }}>
-                
-              <Text
-                style={{
-                  fontSize: 24,
-                  fontWeight: 'bold',
-                  color: 'white',
-                }}>
+              <View>
+                <Modal
+                  animationType="slide"
+                  transparent={true}
+                  visible={modalVisible}
+                  onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    this.setModalVisible(!modalVisible);
+                  }}>
+                  <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                      <Text style={styles.modalText}>Hello World!</Text>
+                      <Text>{this.state.total}</Text>
 
-                CHECKOUT
-              </Text>
-            </TouchableOpacity>
+                      <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => this.setModalVisible(!modalVisible)}>
+                        <Text style={styles.textStyle}>Hide Modal</Text>
+                      </Pressable>
+                    </View>
+                  </View>
+                </Modal>
+                <Pressable
+                  style={{
+                    backgroundColor: '#7f3166',
+                    width: width - 40,
+                    alignItems: 'center',
+                    padding: 10,
+                    borderRadius: 5,
+                    margin: 20,
+                  }}
+                  onPress={() => this.setModalVisible(true)}>
+                  <Text>Check Out</Text>
+                </Pressable>
+              </View>
 
-            <View style={{height: 20}} />
-          </ScrollView>
+              <View style={{height: 20}} />
+            </ScrollView>
+          </View>
         </View>
-      </View>
       </ImageBackground>
     );
   }
 }
-
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: 200,
+    height: 400,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
